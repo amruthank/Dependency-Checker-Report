319d05c7e1ad44c3444b66def7fead3e7b1605fb
Amrutha NK <amruthank196@gmail.com>
2018-09-18 20:10:30 +0530

    Created SeleniumDriver class.

--- Source/extract_license_urls.py
+++ Source/extract_license_urls.py
@@ -3,48 +3,65 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import re
+import multiprocessing
 
 
-res_dict = {}
+
+class SeleniumDriver:
+
+    def __init__(self):
+        self.browser = self.open_browser()
+        
+
+    def open_browser(self):
+        #TODO: raise exceptions on newtwork issues
+        try:
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless') #Headless browser.
+            return webdriver.Chrome(chrome_options=options)
+        except ImportError:
+            raise Exception("Error loading the package selenium!!\t Please install the package.\n")
+        except Exception:
+            raise Exception("Error loading headerless browser package (Chrome browser).")
 
 
-def _get_driver():
+    def get_urls(self, oss_component):
+
+        result_dict = {}
+
+        result_dict["%s"%oss_component] = []
+        
+        url = "https://www.google.com/search?ei=cFNMW7CjA8LQvgSQsICIBA&q=license+of+%s+or+%s+github"%(oss_component, oss_component)
+
+        try:
+            self.browser.get(url)
+        except Exception as e:
+            print(e)
+        driver_sub_element = self.browser.find_elements_by_css_selector('div.g')
+
+        for res in range(0, 3):
+            try:
+                link = driver_sub_element[res].find_element_by_tag_name("a")
+            except Exception:
+                continue
+            else:
+                result_dict["%s"%oss_component].append(link.get_attribute("href"))
+
+        return result_dict
     
-    try:
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless') #Headless browser.
-        driver = webdriver.Chrome(chrome_options=options)
-    except ImportError:
-        raise Exception("Error loading the package selenium!!\t Please install the package.\n")
-    except Exception:
-        raise Exception("Error loading headerless browser package (Chrome browser).")
-    else:
-        return driver
+            
+    def close_browser(self):
+        self.browser.quit()
 
 
-def web_init(oss_component):
-
-    result_dict = {}
-    result_dict["%s"%oss_component] = []
-    
-    url = "https://www.google.com/search?ei=cFNMW7CjA8LQvgSQsICIBA&q=license+of+%s+or+%s+github"%(oss_component, oss_component)
-
-    driver = _get_driver()
-    try:
-        driver.get(url)
-    except Exception as e:
-        print(e)
-    driver_sub_element = driver.find_elements_by_css_selector('div.g')
-
-    for res in range(0, 3):
-        link = driver_sub_element[res].find_element_by_tag_name("a")
-        href = link.get_attribute("href")
-        result_dict["%s"%oss_component].append(href)
-
-    driver.quit()
-    
-    return result_dict
 
 
 if __name__ == "__main__":
-    print(web_init("abbrev"))
+
+    data = ["abbrev", "jquery", "angularjs"]
+    automation = SeleniumDriver()
+    print(automation)
+    for d in data:
+        print(automation.get_urls(d))
+    automation.close_browser()
+    
